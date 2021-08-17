76354c37bdf3e1b84aef667d616c7219124dd1e1
Louis Thanawin <kannaphongi62@nu.ac.th>
2021-08-18 03:58:20 +0700

    includes

---
+++ includes/footer.php
@@ -0,0 +1,9 @@
+<footer class="main-footer">
+    <div class="container">
+      <div class="pull-right hidden-xs">
+        <b>CSIT NU</b>
+      </div>
+      <strong>Made in Phitsanulok ❤❤❤</strong>
+    </div>
+    <!-- /.container -->
+</footer>
